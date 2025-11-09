dec526353dc67e6854da3cf579f26f4affb545c1
Ramakant <ramakant.c@gmail.com>
2025-11-09 19:23:10 +0800

    added 2 functions for loading and pre-processing the data

--- sourcefiles/data_ingestion.py
+++ sourcefiles/data_ingestion.py
@@ -8,10 +8,10 @@ import yaml
 log_dir = 'logs'
 os.makedirs(log_dir, exist_ok=True)
 
-### Configure logging
-logger = logging.getLogger('data_ingestion')
-logger.setLevel('DEBUG')
-
+### Configure logging for logging events in data ingestion
+logger = logging.getLogger('data_ingestion')        # Create a logger for data ingestion by the name 'data_ingestion', nothing related to .py file name
+logger.setLevel('DEBUG')                            # Set the logging level to DEBUG, have levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
+                                                    
 console_handler = logging.StreamHandler()
 console_handler.setLevel('DEBUG')
 
@@ -24,4 +24,37 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+### Function to load data, data can be from local or remote location
+def load_data(data_url: str) -> pd.DataFrame:
+    """Load data from a CSV file into a pandas DataFrame."""
+    try:
+        logger.info(f"Loading data from {data_url}")
+        data = pd.read_csv(data_url)
+        logger.info(f"Data loaded successfully with shape {data.shape}")
+        return data
+    except pd.errors.ParserError as e:
+        logger.error(f"Parsing error while loading data: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Error loading data: {e}")
+        raise
+
+### Function to preprocess data, specific to the dataset being used
+def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
+    """Preprocess the data (example: handle missing values, drop columns, rename columns etc.)."""
+    try:
+        logger.info("Starting data preprocessing")
+        data.drop(columns = ['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], inplace = True) # Drop unnecessary columns
+        data.rename(columns = {'v1': 'target', 'v2': 'text'}, inplace = True)           # Rename columns for better understanding
+        # data = data.dropna()                                                          # Drop rows with missing values
+        # logger.info(f"Data after dropping missing values has shape {data.shape}")
+        logger.info("Data preprocessing completed")
+        return data
+    except KeyError as e:
+        logger.error(f"Key error during data preprocessing: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Error in data preprocessing: {e}")
+        raise
 
